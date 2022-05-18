62aa5fbcec1fbda9b12057df44bd2acda747ec8c
Guilherme Pereira Juventino <guilhermejuventino@gmail.com>
2022-05-18 12:52:52 -0300

    Página básica de login
    Página que pega o nome de usuário e a senha e imprime ela no terminal.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Pegando input do usuário com nome = Username no HTML form
+        username = request.form.get("Username")
+
+        # Pegando input do usuário com nome = Password no HTML form
+        password = request.form.get("Password")
+
+        print(f"Seu nome de usuário é {username}\nSua senha é {password}")
+
     return render_template("index.html")
 
 
