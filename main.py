89ab2b69e419093e3b52b405e504d0daf220439e
Guilherme Pereira Juventino <guilhermejuventino@gmail.com>
2022-05-19 13:44:48 -0300

    Simple redirecting.
    Made a few changes to the html and css, and also added a way to redirect the user to another page upon logging in.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -14,7 +14,15 @@ def login():
 
         print(f"Seu nome de usuário é {username}\nSua senha é {password}")
 
+        # Redireciona para a página expecificada.
+        return redirect(url_for("test"))
+
     return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 
 if __name__ == "__main__":
